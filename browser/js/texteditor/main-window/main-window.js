8976231e60202900b8d68677dd4dd6c7696d7d14
Sam Narisi <snarisi@gmail.com>
2016-01-21 16:21:40 -0500

    Finished style skeleton

---
+++ browser/js/texteditor/main-window/main-window.js
@@ -0,0 +1,9 @@
+app.directive('mainTextEditorWindow', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/texteditor/main-window/main-window.html',
+        link: function (scope, element, attrs) {
+
+        }
+    }
+})
--- browser/js/texteditor/texteditor.js
+++ browser/js/texteditor/texteditor.js
@@ -2,9 +2,10 @@ app.directive('textEditor', function () {
     return {
         restrict: 'E',
         templateUrl: 'js/texteditor/texteditor.html',
-        scope: {},
         link: function (scope, element, attrs) {
-
+            scope.save = function (file) {
+                console.log('file: ', file)
+            }
         }
     }
 });
